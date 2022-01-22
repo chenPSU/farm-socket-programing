044e89d1e1183ac96160b0186a896d95823d7abb
churnPSU <churngg@gmail.com>
2022-01-22 17:05:32 +0700

    socket v1

--- socket.js
+++ socket.js
@@ -8,19 +8,22 @@ net.createServer(function (sock) {
     var state = 0 //idle
     var current_key = null    
     sock.on('data', function (data) {
+        console.log("round");
         switch(state){
             case 0:
                 if(data == 'HELLO'){
-                    sock.write('HELLO')
+                    sock.write('HELLOS')
                     state = 1 //wait for key
                 }
                 break
             case 1:
+                console.log(1);
                 current_key = data
                 sock.write("" + (db[current_key] || 0))
                 state = 2 //wait for number
                 break
             case 2:
+                console.log(2);
                 if(data == 'BYE'){
                     sock.close()
                     state = 3 //end                    
